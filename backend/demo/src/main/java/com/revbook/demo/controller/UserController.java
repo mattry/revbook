9750d386f7e1deb31f784d0b0752710152b2aa1a
Matt <84721683+mattry@users.noreply.github.com>
2024-12-13 16:51:04 -0500

    Add CrossOrigin with localhost:3000 to UserController to allow requests from our React frontend.

--- backend/demo/src/main/java/com/revbook/demo/controller/UserController.java
+++ backend/demo/src/main/java/com/revbook/demo/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class UserController {
 
