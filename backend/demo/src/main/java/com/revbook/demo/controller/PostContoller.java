c6d789bf3e488d0ebc3023c4cfae90415294d586
Matt <84721683+mattry@users.noreply.github.com>
2024-12-13 17:39:10 -0500

    Create PostController with createPost endpoint for users to create posts

---
+++ backend/demo/src/main/java/com/revbook/demo/controller/PostContoller.java
@@ -0,0 +1,33 @@
+package com.revbook.demo.controller;
+
+import com.revbook.demo.exception.InvalidInputException;
+import com.revbook.demo.service.PostService;
+import com.revbook.demo.entity.Post;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http:localhost/3000")
+@RestController
+public class PostContoller {
+
+    @Autowired
+    private PostService postService;
+
+    @PostMapping("/posts")
+    public ResponseEntity<Post> createPost(@RequestBody Post post) {
+        try{
+            Post createdPost = postService.createPost(post);
+            return ResponseEntity.ok(createdPost);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+    }
+
+
+
+}
