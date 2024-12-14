2c7d26c9b9ed7febaceef51684a757a54b68e7f1
Matt <84721683+mattry@users.noreply.github.com>
2024-12-14 16:56:47 -0500

    Add method to find comments based on their postId

--- backend/demo/src/main/java/com/revbook/demo/repository/CommentRepository.java
+++ backend/demo/src/main/java/com/revbook/demo/repository/CommentRepository.java
@@ -3,5 +3,9 @@ package com.revbook.demo.repository;
 import com.revbook.demo.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Optional<List<Comment>> findByPost_PostId(Long postId);
 }
