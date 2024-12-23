9e211c17e58d8db6c4b23a385bb7531548328ba3
Matt <84721683+mattry@users.noreply.github.com>
2024-12-23 14:56:33 -0500

    Add HttpSession to params on all user controller methods, allowCredentials set to true."

--- backend/demo/src/main/java/com/revbook/demo/controller/UserController.java
+++ backend/demo/src/main/java/com/revbook/demo/controller/UserController.java
@@ -8,6 +8,7 @@ import com.revbook.demo.exception.EmailAlreadyInUseException;
 import com.revbook.demo.exception.InvalidInputException;
 import com.revbook.demo.service.UserService;
 import com.revbook.demo.entity.User;
+import jakarta.servlet.http.HttpSession;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,7 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 @RestController
 public class UserController {
 
@@ -23,9 +24,11 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<UserDTO> registerUser(@RequestBody UserAuthDTO userAuthDTO) {
+    public ResponseEntity<UserDTO> registerUser(@RequestBody UserAuthDTO userAuthDTO, HttpSession session) {
         try {
             UserDTO registeredUserDTO = userService.registerUser(userAuthDTO);
+            session.setAttribute("userId", registeredUserDTO.getUserId());
+            session.setAttribute("userPassword", userAuthDTO.getPassword());
             return ResponseEntity.ok(registeredUserDTO);
         } catch (EmailAlreadyInUseException | InvalidInputException e){
             return ResponseEntity.status(HttpStatus.CONFLICT).body(null);
@@ -33,9 +36,11 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<UserDTO> login(@RequestBody UserAuthDTO userAuthDTO) {
+    public ResponseEntity<UserDTO> login(@RequestBody UserAuthDTO userAuthDTO,  HttpSession session) {
         try{
             UserDTO authUserDTO = userService.authenticate(userAuthDTO);
+            session.setAttribute("userId", authUserDTO.getUserId());
+            session.setAttribute("userPassword", userAuthDTO.getPassword());
             return ResponseEntity.ok(authUserDTO);
         } catch (InvalidInputException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
@@ -43,7 +48,11 @@ public class UserController {
     }
 
     @PatchMapping("/update-name")
-    public ResponseEntity<UserDTO> updateName(@RequestBody UpdateNameDTO updateNameDTO) {
+    public ResponseEntity<UserDTO> updateName(@RequestBody UpdateNameDTO updateNameDTO, HttpSession session) {
+        Long userId = (Long) session.getAttribute("userId");
+        if (userId == null){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
         try {
             UserDTO updatedUser = userService.updateName(updateNameDTO);
             return ResponseEntity.ok(updatedUser);
@@ -53,7 +62,12 @@ public class UserController {
     }
 
     @PatchMapping("/update-password")
-    public ResponseEntity<Void> updatePassword(@RequestBody UpdatePasswordDTO updatePasswordDTO) {
+    public ResponseEntity<Void> updatePassword(@RequestBody UpdatePasswordDTO updatePasswordDTO, HttpSession session) {
+        Long userId = (Long) session.getAttribute("userId");
+        if (userId == null){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
         try {
             userService.updatePassword(updatePasswordDTO);
             return ResponseEntity.ok().build();
@@ -63,7 +77,11 @@ public class UserController {
     }
 
     @GetMapping("/users/{userId}")
-    public ResponseEntity<UserDTO> getUserById(@PathVariable Long userId){
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Long userId, HttpSession session){
+        Long sessionUserId = (Long) session.getAttribute("userId");
+        if (sessionUserId == null){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
         try {
             UserDTO userDTO = userService.getUserById(userId);
             return ResponseEntity.ok(userDTO);
